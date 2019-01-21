435db0c75fc0b920446b40b2516f38bf5852268e
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-21 15:21:57 +0100

    Load easier all tables

--- src/createTables.js
+++ src/createTables.js
@@ -1,15 +1,22 @@
-import Product from './models/Product'
+import Models from './models/index';
+import sequelize from './services/sequelize';
+
+sequelize.getInstance();
 
 async function load () {
-  await new Promise(resolve => {
-    Product.sync({ force: true }).then(() => {
-      console.info('Table Product created !');
-      resolve();
-    }).catch(error => {
-      console.error('Unable to create Table Product !', error);
-      resolve();
+  const loading = Object.keys(Models).map(async key => {
+    await new Promise(resolve => {
+      Models[key].sync({ force: true }).then(() => {
+        console.info(`Table ${key} created !`);
+        resolve();
+      }).catch(error => {
+        console.error(`Unable to create Table ${key} !`, error);
+        resolve();
+      });
     });
   });
+
+  await Promise.all(loading);
 
   process.exit(0);
 }
