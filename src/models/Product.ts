dbb89114320f8be7d9951df041b19e2be2648047
Alex Chopard <alex.chopard1998@gmail.com>
2019-02-25 11:47:59 +0100

    Rename table in db

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,7 +2,7 @@
 import Sequelize = require("sequelize");
 import sequelize = require("../services/sequelize");
 
-export const model = sequelize.getInstance().define("product", {
+export const model = sequelize.getInstance().define("Product", {
   name: {
     type: Sequelize.STRING
   }
--- src/models/TypePlat.ts
+++ src/models/TypePlat.ts
@@ -2,6 +2,6 @@
 import Sequelize = require("sequelize");
 import sequelize = require("../services/sequelize");
 
-export const model = sequelize.getInstance().define("typePlat", {
+export const model = sequelize.getInstance().define("TypePlat", {
   libelle: { type: Sequelize.STRING(50), allowNull: false }
 });
