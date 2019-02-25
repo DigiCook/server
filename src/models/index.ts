8fb8e41c096bb81c894cfd93f34b1a3ffdda2d71
Alex Chopard <alex.chopard1998@gmail.com>
2019-02-25 14:45:38 +0100

    Add Adresse model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export const Product = require("./Product").Product;
 export const TypePlat = require("./TypePlat").TypePlat;
 export const Plat = require("./Plat").Plat;
+export const Adresse = require("./Adresse").Adresse;
