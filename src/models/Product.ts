20a228f1a5da28e8f80c803bfffa1e72a551a8d8
Alex-Chopard <alex.chopard1998@gmail.com>
2019-03-19 10:11:24 +0100

    Remove useless file

--- src/models/Product.ts
+++
@@ -1,9 +0,0 @@
-
-import Sequelize = require("sequelize");
-import sequelize = require("../services/sequelize");
-
-export const Product = sequelize.getInstance().define("Product", {
-  name: {
-    type: Sequelize.STRING
-  }
-});
--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,6 @@ export const Adresse = require("./Adresse").Adresse;
 export const Restaurant = require("./Restaurant").Restaurant;
 export const Personne = require("./Personne").Personne;
 export const Plat = require("./Plat").Plat;
-export const Product = require("./Product").Product;
 export const Exploitation = require("./Exploitation").Exploitation;
 export const Commande = require("./Commande").Commande;
 export const CommandePlat = require("./CommandePlat").CommandePlat;
