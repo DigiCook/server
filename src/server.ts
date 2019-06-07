620dc7a50b9a1819fb0607707337d4b42c5c0996
Alex-Chopard <alex.chopard1998@gmail.com>
2019-06-07 11:11:56 +0200

    Remove the live from this server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import bodyParser = require("body-parser");
-import child = require("child_process");
 import cors = require("cors");
 import dotenv = require("dotenv");
-import events = require("events");
 import express = require("express");
-// import Stream = require("node-rtsp-stream");
 import router = require("./router");
 import sequelize = require("./services/sequelize");
 import socket = require("./services/socket");
@@ -30,70 +27,6 @@ app.use("/api", (req, res, next) => {
 });
 
 router.load(app);
-// TMP
-let clientNum: number = 1;
-
-function startffmpeg(clientNumer: number) {
-  console.log("Starting FFMPEG for client " + clientNumer);
-  let ffmpegString = "-i rtsp://193.168.1.10/11 -f webm -c:v libvpx -an -";
-  if (ffmpegString.indexOf("rtsp://") > -1) {
-      ffmpegString = `-rtsp_transport tcp ${ffmpegString}`;
-  }
-  console.log(`Executing : ffmpeg ${ffmpegString}`);
-  const ffmpegLive = child.spawn("ffmpeg", ffmpegString.split(" "));
-  ffmpegLive.on("close", (buffer) => {
-      console.log("ffmpeg died", buffer);
-  });
-
-  const emitter = new events.EventEmitter().setMaxListeners(1);
-  ffmpegLive.stdout.on("data", (buffer) => {
-      emitter.emit("data", buffer);
-  });
-
-  return { ffmpegLive, emitter };
-}
-
-app.get("/live", (req, res) => {
-  const thisclientNum: number = clientNum++;
-  const result: any = startffmpeg(thisclientNum);
-  const { ffmpegLive, emitter } = result;
-  const contentWriter = (buffer) => {
-    res.write(buffer);
-  };
-
-  res.writeHead(200, {
-      "Date": new Date().toUTCString(),
-      "Connection": "close",
-      "Cache-Control": "no-cache",
-      "Pragma": "no-cache",
-      "Content-Type": "video/webm",
-      "Server": "WebM from rtsp stream",
-  });
-
-  emitter.on("data", contentWriter);
-
-  res.on("close", () => {
-      emitter.removeListener("data", contentWriter);
-      console.log("Connection closed by client " + thisclientNum);
-      if (ffmpegLive) {
-        ffmpegLive.kill();
-        console.log("ffmpeg being killed....");
-      } else {
-        console.log("no ffmpeg existing");
-      }
-  });
-});
-
-/* const a = new Stream({
-  name: "name",
-  streamUrl: "rtsp://admin:admin@193.168.1.10/11",
-  wsPort: 9998,
-  ffmpegOptions: { // options ffmpeg flags
-    "-stats": "", // an option with no neccessary value uses a blank string
-    "-r": 30 // options with required values specify the value after the key
-  }
-}); */
-// TMP
 
 // init the socket server and express server on the same port !
 socket.init(app);
